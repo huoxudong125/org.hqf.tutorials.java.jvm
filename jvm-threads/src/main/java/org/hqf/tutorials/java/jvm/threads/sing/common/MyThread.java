9de916fc84afe2a727e891b9bd4a1969cd865483
huoxudong125 <423huo@163.com>
2020-04-06 13:08:47 +0800

    Add Thread Test

---
+++ jvm-threads/src/main/java/org/hqf/tutorials/java/jvm/threads/sing/common/MyThread.java
@@ -0,0 +1,18 @@
+package org.hqf.tutorials.java.jvm.threads.sing.common;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MyThread extends Thread {
+    //run()方法里是线程体
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(this.getName() + ":" + i);//getName()方法是返回线程名称
+
+            try {
+                Thread.sleep(ThreadLocalRandom.current().nextInt(10)*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ jvm-threads/src/main/java/org/hqf/tutorials/java/jvm/threads/sing/common/MyThreadWithName.java
@@ -0,0 +1,17 @@
+package org.hqf.tutorials.java.jvm.threads.sing.common;
+
+public class MyThreadWithName {
+    public static void main(String[] args) {
+        System.out.println("Main Function executed Start...");
+
+        Thread t=new MyThread();
+        t.setName("HQF-Single-Thread-1");
+        t.start();
+
+        Thread t2=new MyThread();
+        t2.setName("HQF-Single-Thread-2");
+        t2.start();
+
+        System.out.println("Main Function executed Finished");
+    }
+}
